c27f3a107bc9c1634e235e828063ef1f2d2d112f
Michael Bikovitsky <biko@bikodbg.com>
2022-12-17 10:51:03 +0200

    Optimize day 16 part 1

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,48 +1,114 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use anyhow::{Context, Result};
 use aoc2022::util::input_lines;
 use itertools::{iproduct, Itertools};
 use lazy_static::lazy_static;
 use petgraph::{
-    algo::{bellman_ford, floyd_warshall},
-    graph::DiGraph,
+    algo::floyd_warshall,
+    graph::{DiGraph, NodeIndex},
     graphmap::DiGraphMap,
+    visit::EdgeRef,
+    Direction,
 };
 use regex::Regex;
 
 fn main() -> Result<()> {
     let valves = parse_input()?;
 
-    let most_pressure = compute_most_pressure(&valves, 30);
+    let most_pressure = compute_most_pressure(&valves, 30, "AA");
     dbg!(most_pressure);
 
     Ok(())
 }
 
-fn compute_most_pressure(valves: &[Valve], end_time: u32) -> u32 {
-    // For the bitmap
-    assert!(valves.len() < 64);
-
+fn compute_most_pressure(valves: &[Valve], total_time: u32, start_valve: &str) -> u32 {
     // First, build a map of the tunnel system
-    let map = {
-        // Directed graph because `floyd_warshall` doesn't like undirected ones...
-        let mut map: DiGraphMap<&str, ()> = DiGraphMap::new();
-        for valve in valves {
-            for neighbour in &valve.tunnels {
-                map.add_edge(&valve.name, neighbour, ());
-            }
+    let (map, start) = compress_valve_map(valves, start_valve);
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    struct State {
+        time: u32,
+        position: NodeIndex,
+        turned_on_valves: u64,
+        utility: u32, // How much total pressure will be released when time runs out
+    }
+
+    let mut to_visit = vec![State {
+        time: 0,
+        position: start,
+        turned_on_valves: 0,
+        utility: 0,
+    }];
+
+    let mut discovered = HashSet::new();
+
+    let mut best_utility = 0;
+
+    while let Some(current) = to_visit.pop() {
+        if !discovered.insert(current) {
+            continue;
         }
-        map
-    };
+
+        best_utility = best_utility.max(current.utility);
+
+        // Find valves we can turn on
+        for edge in map.edges_directed(current.position, Direction::Outgoing) {
+            // Is this valve already turned on?
+            if current.turned_on_valves & (1 << edge.target().index()) != 0 {
+                continue;
+            }
+
+            // No sense in turning on valves that don't contribute anything
+            let flow_rate = *map.node_weight(edge.target()).unwrap();
+            if flow_rate == 0 {
+                continue;
+            }
+
+            // New time is after moving to the new valve and turning it on
+            let new_time = current.time + edge.weight() + 1;
+            if new_time >= total_time {
+                continue;
+            }
+
+            let new_state = State {
+                time: new_time,
+                position: edge.target(),
+                turned_on_valves: current.turned_on_valves | (1 << edge.target().index()),
+                utility: current.utility + (total_time - new_time) * flow_rate,
+            };
+
+            to_visit.push(new_state);
+        }
+    }
+
+    best_utility
+}
+
+fn compress_valve_map(valves: &[Valve], start_valve: &str) -> (DiGraph<u32, u32>, NodeIndex) {
+    // Directed graph because `floyd_warshall` doesn't like undirected ones...
+    let mut map: DiGraphMap<&str, ()> = DiGraphMap::new();
+    for valve in valves {
+        for neighbour in &valve.tunnels {
+            map.add_edge(&valve.name, neighbour, ());
+        }
+    }
+
+    // We only care about the working valves, and the starting one,
+    // so let's keep just them
+
+    let mut compressed = DiGraph::new();
 
     let shortest_paths = floyd_warshall(&map, |_| 1u32).unwrap();
 
-    let working_valves_indices = valves
+    let interesting_valves_indices = valves
         .iter()
         .enumerate()
         .filter_map(|(index, valve)| {
-            if valve.flow_rate != 0 {
+            if valve.name == start_valve || valve.flow_rate != 0 {
                 Some(index)
             } else {
                 None
@@ -50,113 +116,41 @@ fn compute_most_pressure(valves: &[Valve], end_time: u32) -> u32 {
         })
         .collect_vec();
 
-    let turned_on_combos = working_valves_indices
+    let valve_index_to_node_index: HashMap<_, _> = interesting_valves_indices
         .iter()
-        .powerset()
-        .map(|indices| {
-            indices
-                .into_iter()
-                .map(|index| 1u64 << index)
-                .fold(0, |acc, val| acc | val)
+        .map(|&valve_index| {
+            let node_index = compressed.add_node(valves[valve_index].flow_rate);
+            assert!(node_index.index() < 64); // For the bitmap
+            (valve_index, node_index)
         })
-        .collect_vec();
+        .collect();
 
-    // Build a graph of possible states
-    // State: (current time, current position, bitmap of turned on valves)
-    // Weight: Increase in total pressure at end time if we take this edge, negated
-    let (state_graph, start_state) = {
-        let mut state_graph: DiGraph<(u32, usize, u64), f64> = DiGraph::new();
+    for (&a, &b) in iproduct!(&interesting_valves_indices, &interesting_valves_indices) {
+        if a == b {
+            continue;
+        }
 
-        let mut node_indices = HashMap::new();
+        let distance = shortest_paths[&(valves[a].name.as_str(), valves[b].name.as_str())];
 
-        let start_state = (
-            0,
-            valves
-                .iter()
-                .find_position(|valve| valve.name == "AA")
-                .unwrap()
-                .0,
-            0,
+        if distance == u32::MAX {
+            continue;
+        }
+
+        compressed.add_edge(
+            valve_index_to_node_index[&a],
+            valve_index_to_node_index[&b],
+            distance,
         );
+    }
 
-        let all_states = || {
-            std::iter::once(start_state).chain(iproduct!(
-                0..end_time,
-                working_valves_indices.iter().copied(),
-                turned_on_combos.iter().copied()
-            ))
-        };
-
-        for current_state in all_states() {
-            node_indices.insert(current_state, state_graph.add_node(current_state));
-        }
-
-        for (time, position, turned_on_valves) in all_states() {
-            let current_state = node_indices[&(time, position, turned_on_valves)];
-
-            for &new_position in &working_valves_indices {
-                // No sense in turning on valves that are already turned on
-                if turned_on_valves & (1 << new_position) != 0 {
-                    continue;
-                }
-
-                let distance = shortest_paths[&(
-                    valves[position].name.as_str(),
-                    valves[new_position].name.as_str(),
-                )];
-
-                // Filter out unreachable valves
-                if distance == u32::MAX {
-                    continue;
-                }
-
-                let new_time = time + distance + 1;
-                if new_time >= end_time {
-                    continue;
-                }
-
-                let new_state = (
-                    new_time,
-                    new_position,
-                    turned_on_valves | (1 << new_position),
-                );
-
-                // How much pressure release can we get?
-                let utility = (end_time - new_time) * valves[new_position].flow_rate;
-
-                // Negate it, to employ shortest path search
-                let utility: f64 = utility.into();
-                let utility = -utility;
-
-                let new_state = node_indices[&new_state];
-                state_graph.add_edge(current_state, new_state, utility);
-            }
-        }
-
-        (state_graph, node_indices[&start_state])
-    };
-
-    let paths = bellman_ford(&state_graph, start_state).unwrap();
-
-    paths
-        .distances
+    let start = interesting_valves_indices
         .into_iter()
-        .filter_map(|pressure| {
-            let pressure = -pressure;
+        .filter(|&index| valves[index].name == start_valve)
+        .exactly_one()
+        .expect("Expected to find exactly one start valve 'AA'");
+    let start = valve_index_to_node_index[&start];
 
-            assert!(!pressure.is_nan());
-
-            if pressure.is_infinite() {
-                return None;
-            }
-
-            assert!(0.0 <= pressure && pressure <= u32::MAX.into());
-
-            let pressure: u32 = unsafe { pressure.to_int_unchecked() };
-            Some(pressure)
-        })
-        .max()
-        .unwrap()
+    (compressed, start)
 }
 
 #[derive(Debug, Clone)]
