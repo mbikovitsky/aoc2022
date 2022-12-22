f3307e6f4233dcdde74195ae58670cef4ffb838b
Michael Bikovitsky <biko@bikodbg.com>
2022-12-22 11:15:46 +0200

    Implement day 21 part 2

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -4,7 +4,11 @@ use anyhow::{bail, Result};
 use aoc2022::util::input_lines;
 use itertools::Itertools;
 use lazy_static::lazy_static;
-use petgraph::{graph::DiGraph, visit::DfsPostOrder, Direction};
+use petgraph::{
+    graph::DiGraph,
+    visit::{Dfs, DfsPostOrder},
+    Direction,
+};
 use regex::Regex;
 
 fn main() -> Result<()> {
@@ -13,10 +17,13 @@ fn main() -> Result<()> {
     let result = compute(&monkeys, "root");
     dbg!(result);
 
+    let unknown = compute_unknown(&monkeys, "root", "humn");
+    dbg!(unknown);
+
     Ok(())
 }
 
-fn compute(monkeys: &[Monkey], start: &str) -> i64 {
+fn compute(monkeys: &[Monkey], root: &str) -> i64 {
     let mut graph = DiGraph::new();
     let mut name_to_node_index = HashMap::new();
 
@@ -50,21 +57,21 @@ fn compute(monkeys: &[Monkey], start: &str) -> i64 {
         }
     }
 
-    let mut dfs = DfsPostOrder::new(&graph, name_to_node_index[start]);
+    let mut dfs = DfsPostOrder::new(&graph, name_to_node_index[root]);
     while let Some(node) = dfs.next(&graph) {
-        let computed = match graph.node_weight(node).unwrap() {
+        let computed = match &graph[node] {
             Node::Value(value) => *value,
             Node::Op(op) => {
                 let (left, right) = graph
                     .neighbors_directed(node, Direction::Outgoing)
                     .collect_tuple()
                     .expect("Expected exactly two neighbours");
-                let left = if let Node::Value(value) = graph.node_weight(left).unwrap() {
+                let left = if let Node::Value(value) = &graph[left] {
                     *value
                 } else {
                     panic!("Uncomputed left neighbour");
                 };
-                let right = if let Node::Value(value) = graph.node_weight(right).unwrap() {
+                let right = if let Node::Value(value) = &graph[right] {
                     *value
                 } else {
                     panic!("Uncomputed right neighbour");
@@ -75,16 +82,179 @@ fn compute(monkeys: &[Monkey], start: &str) -> i64 {
                     Operation::Sub => left - right,
                     Operation::Mul => left * right,
                     Operation::Div => left / right,
+                    Operation::Eq => panic!("There shouldn't be an = operator"),
                 }
             }
         };
 
-        *graph.node_weight_mut(node).unwrap() = Node::Value(computed);
+        graph[node] = Node::Value(computed);
     }
 
-    match graph.node_weight(name_to_node_index[start]).unwrap() {
+    match &graph[name_to_node_index[root]] {
         Node::Value(value) => *value,
         _ => panic!("Didn't compute node"),
+    }
+}
+
+fn compute_unknown(monkeys: &[Monkey], root: &str, unknown: &str) -> i64 {
+    let mut graph = DiGraph::new();
+    let mut name_to_node_index = HashMap::new();
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    struct Node {
+        value: Option<i64>,
+        op: Option<Operation>,
+    }
+
+    for monkey in monkeys {
+        let node = if monkey.name == root {
+            Node {
+                value: None,
+                op: Some(Operation::Eq),
+            }
+        } else if monkey.name == unknown {
+            Node {
+                value: None,
+                op: None,
+            }
+        } else {
+            match monkey.job {
+                Job::Const(value) => Node {
+                    value: Some(value),
+                    op: None,
+                },
+                Job::Compute { op, .. } => Node {
+                    value: None,
+                    op: Some(op),
+                },
+            }
+        };
+
+        let node_index = graph.add_node(node);
+
+        let inserted = name_to_node_index
+            .insert(monkey.name.as_str(), node_index)
+            .is_none();
+        assert!(inserted);
+    }
+
+    for monkey in monkeys {
+        if let Job::Compute { left, right, .. } = &monkey.job {
+            let current = name_to_node_index[monkey.name.as_str()];
+            let left = name_to_node_index[left.as_str()];
+            let right = name_to_node_index[right.as_str()];
+            // For a directed graph, neighbors_directed returns nodes in reverse order
+            // of insertion
+            graph.add_edge(current, right, ());
+            graph.add_edge(current, left, ());
+        }
+    }
+
+    // 1st pass: traverse the tree bottom-up, and compute as much as possible
+
+    let mut dfs = DfsPostOrder::new(&graph, name_to_node_index[root]);
+    while let Some(node) = dfs.next(&graph) {
+        if graph[node].op.is_none() {
+            continue;
+        }
+
+        assert!(
+            graph[node].value.is_none(),
+            "A compute node shouldn't have an initial value"
+        );
+
+        let (left, right) = graph
+            .neighbors_directed(node, Direction::Outgoing)
+            .collect_tuple()
+            .expect("Expected exactly two neighbours");
+
+        let computed = match (graph[left].value, graph[right].value) {
+            (None, None) => panic!("Detected operation with both branches unknown"),
+
+            (Some(left), Some(right)) => match graph[node].op.unwrap() {
+                Operation::Add => Some(left + right),
+                Operation::Sub => Some(left - right),
+                Operation::Mul => Some(left * right),
+                Operation::Div => Some(left / right),
+                Operation::Eq => {
+                    // Skip the root for now
+                    assert!(node == name_to_node_index[root]);
+                    None
+                }
+            },
+
+            // One of the branches is unknown, so skip for now
+            (None, Some(_)) => None,
+            (Some(_), None) => None,
+        };
+
+        graph[node].value = computed;
+    }
+
+    // 2nd pass: traverse top-down to find the unknown value
+
+    assert!(graph[name_to_node_index[root]].value.is_none());
+    assert!(graph[name_to_node_index[root]].op == Some(Operation::Eq));
+
+    let mut dfs = Dfs::new(&graph, name_to_node_index[root]);
+    while let Some(node) = dfs.next(&graph) {
+        if graph[node].op.is_none() {
+            continue;
+        }
+
+        let (left, right) = graph
+            .neighbors_directed(node, Direction::Outgoing)
+            .collect_tuple()
+            .expect("Expected exactly two neighbours");
+
+        if graph[left].value.is_some() && graph[right].value.is_some() {
+            continue;
+        }
+
+        let computed = solve_for_x(
+            graph[node].op.unwrap(),
+            graph[left].value,
+            graph[right].value,
+            graph[node].value,
+        );
+
+        if graph[left].value.is_none() {
+            graph[left].value = Some(computed);
+        } else {
+            graph[right].value = Some(computed);
+        }
+    }
+
+    graph[name_to_node_index[unknown]]
+        .value
+        .expect("Didn't compute the unknown value")
+}
+
+fn solve_for_x(op: Operation, left: Option<i64>, right: Option<i64>, expected: Option<i64>) -> i64 {
+    if left.is_none() && right.is_none() {
+        panic!("Both operands are unknown");
+    } else if left.is_some() && right.is_some() {
+        panic!("Both operands are known");
+    } else {
+        match op {
+            Operation::Add => expected.unwrap() - left.unwrap_or_else(|| right.unwrap()),
+            Operation::Sub => {
+                if let Some(left) = left {
+                    left - expected.unwrap()
+                } else {
+                    expected.unwrap() + right.unwrap()
+                }
+            }
+            Operation::Mul => expected.unwrap() / left.unwrap_or_else(|| right.unwrap()),
+            Operation::Div => {
+                if let Some(left) = left {
+                    left / expected.unwrap()
+                } else {
+                    expected.unwrap() * right.unwrap()
+                }
+            }
+            Operation::Eq => left.unwrap_or_else(|| right.unwrap()),
+        }
     }
 }
 
@@ -110,6 +280,7 @@ enum Operation {
     Sub,
     Mul,
     Div,
+    Eq,
 }
 
 impl FromStr for Operation {
@@ -121,6 +292,7 @@ impl FromStr for Operation {
             "-" => Ok(Self::Sub),
             "*" => Ok(Self::Mul),
             "/" => Ok(Self::Div),
+            "=" => Ok(Self::Eq),
             _ => bail!("Invalid operation {}", s),
         }
     }
